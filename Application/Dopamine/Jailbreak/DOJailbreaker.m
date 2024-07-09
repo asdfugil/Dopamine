f4f7781106f6d552eaee6eaa297c6356eba0c509
Nick Chan <towinchenmi@gmail.com>
2024-07-09 21:40:25 +0800

    stuff

--- Application/Dopamine/Jailbreak/DOJailbreaker.m
+++ Application/Dopamine/Jailbreak/DOJailbreaker.m
@@ -466,7 +466,7 @@ void *boomerang_server(struct boomerang_info *info)
         return [NSError errorWithDomain:JBErrorDomain code:JBErrorCodeFailedDuplicateApps userInfo:@{ NSLocalizedDescriptionKey : [NSString stringWithFormat:DOLocalizedString(@"Duplicate_Apps_Error_User_App"), duplicateAppsString, dopamineAppsPath]}];
     }
     
-    for (NSString *dopamineAppId in dopamineInstalledAppIds) {
+    /*for (NSString *dopamineAppId in dopamineInstalledAppIds) {
         LSApplicationProxy *appProxy = [LSApplicationProxy applicationProxyForIdentifier:dopamineAppId];
         if (appProxy.installed) {
             NSString *appProxyPath = [[appProxy.bundleURL.path stringByResolvingSymlinksInPath] stringByStandardizingPath];
@@ -474,7 +474,7 @@ void *boomerang_server(struct boomerang_info *info)
                 return [NSError errorWithDomain:JBErrorDomain code:JBErrorCodeFailedDuplicateApps userInfo:@{ NSLocalizedDescriptionKey : [NSString stringWithFormat:DOLocalizedString(@"Duplicate_Apps_Error_Icon_Cache"), dopamineAppId, dopamineAppsPath, appProxy.bundleURL.path]}];
             }
         }
-    }
+    }*/
     
     return nil;
 }
